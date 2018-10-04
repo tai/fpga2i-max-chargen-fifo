e03899616cc17b306403f060931cd3424dc42a8a
Taisuke Yamada <tai@rakugaki.org>
2018-10-05 08:35:01 +0900

    Extended sample to use all wired input/output.

--- top.v
+++ top.v
@@ -1,16 +1,55 @@
-`define CYCLE_1SEC 48000000 // 48MHz
+//
+// LED blink example to test FPGA2I board
+//
+// Expected behavior:
+// - LED[i] will toggle every second
+// - LED[i] will blink when DIP[i] is OFF
+//
 
-//---------------------------------
-// Top pf the FPGA
-//---------------------------------
+`define CYCLE_1SEC 50_000_000 // 50MHz
+
 module top
   (
-   input wire 	     clk, // 48MHzClock
-   input wire 	     res_n, // Reset Switch
-   input wire [2:0]  dip, // DIP switch
-   output wire [2:0] led  // LED Output
+   input wire 	     clk,
+   input wire 	     res_n,
+   input wire [2:0]  dip,
+   output wire [2:0] led
    );
 
-   assign led = dip;
+   reg [32:0] 	     counter;
+   reg [2:0] 	     led_output;
+
+   initial begin
+      counter = 0;
+   end
+
+   always @(posedge clk, negedge res_n) begin
+      // reset
+      if (~res_n) begin
+	 led_output <= ~0;
+	 counter <= 0;
+      end
+
+      // clocked operation
+      else begin
+	 counter <= counter + 1;
+
+	 // flip led output every cycle
+	 if (counter == `CYCLE_1SEC) begin
+	    counter <= 0;
+	    led_output <= ~led_output;
+	 end
+      end
+   end
+
+   // LED[i]:ON when DIP[i]:ON
+   // - led[i] lights when led[i] is 0
+   // - led[i] is 1 when dip[i] is 0 (DIP[i]:OFF)
+   // -- So DIP works as a mask to turn LED OFF
+   // -- Negated logic is to turn LED ON when DIP is OFF
+   assign led = led_output | ~dip;
+
+   // LED[i]:ON when DIP[i]:ON
+   //assign led = dip;
    
 endmodule
